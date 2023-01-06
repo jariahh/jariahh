77bdb15f0e4704e6b71e0992fc1039d86912c053
jariahh <jariahh@gmail.com>
2023-01-06 18:21:55 -0500

    added material and a basic menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import menu from './menu.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'jariahh';
+  mobileQuery: MediaQueryList;
+
+  fillerNav: any[] = [];
+
+  fillerContent = Array.from(
+    {length: 50},
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.getMenu(menu, this.fillerNav);
+    console.log(this.fillerNav);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  private getMenu(menu: any[], results: any[], navPath: string[] = ['/'], depth = 0) {
+    menu.forEach(m => {
+      results.push({...m, navPath: [...navPath, m.path], depth});
+      if(m.children) {
+        this.getMenu(m.children, results, [...navPath, m.path], depth + 1);
+      }
+    })
+  }
 }
