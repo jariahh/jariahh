7e7e09c7be1ea2f6f06d3141420e4109a2f2d8fd
jariahh <jariahh@gmail.com>
2023-02-16 11:25:39 -0500

    fixing path to default file

--- API/bin/server.dart
+++ API/bin/server.dart
@@ -9,7 +9,7 @@ Future<void> main() async {
 }
 Future<HttpServer> createServer() async {
   final address = '0.0.0.0';
-  const port = 8080;
+  const port = 80;
   return await HttpServer.bind(address, port);
 }
 Future<void> handleRequests(HttpServer server) async {
