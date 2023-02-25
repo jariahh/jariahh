86ce02b43ca1b2e1eca0ed11b85c138dbaf34d11
jariahh <jariahh@gmail.com>
2023-02-25 10:49:56 -0500

    fixing some layout

--- UI/projects/viewer/src/app/theme-viewer/material-components/form-field/form-field.component.ts
+++ UI/projects/viewer/src/app/theme-viewer/material-components/form-field/form-field.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ThemeService } from '../../../../../../../src/app/shared/services/theme.service';
+import { Options } from '../../../../../../../src/app/style.service';
 
 @Component({
   selector: 'app-form-field',
   templateUrl: './form-field.component.html',
   styleUrls: ['./form-field.component.scss']
 })
-export class FormFieldComponent {
+export class FormFieldComponent implements OnInit{
+  public theme: Options = new Options();
+  constructor(private themeService: ThemeService) {
+  }
+  public ngOnInit(): void {
+
+    this.themeService.currentTheme$
+      .pipe(tap(theme => {
+        this.theme = theme;
+      }))
+      .subscribe();
+  }
 
 }
